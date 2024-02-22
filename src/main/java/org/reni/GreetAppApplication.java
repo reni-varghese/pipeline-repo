f7aff20c666ffda98a5e142a3de34e7c197ebaf8
Reni Varghese <reni.varghese@gmail.com>
2024-02-23 02:33:05 +0530

    first commit

---
+++ src/main/java/org/reni/GreetAppApplication.java
@@ -0,0 +1,22 @@
+package org.reni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GreetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetAppApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String greet() {
+		return "Hello from Jenkins";
+	}
+	
+
+}
